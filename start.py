7df0a0dbc6ad9f09a3a1235fb1942aed59724d79
ali <alibabajani1@outlook.com>
2024-09-16 21:54:20 +0330

    delete a while loop

--- start.py
+++ start.py
@@ -1,8 +1,7 @@
 a = 1
-while True:
-    size = input("enter a number(it will be 1 to your number):")
-    size = int(size)
-    size += 1
-    while a < size:
-        print(a)
-        a += 1
+size = input("enter a number(it will be 1 to your number):")
+size = int(size)
+size += 1
+while a < size:
+    print(a)
+    a += 1
