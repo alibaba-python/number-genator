c6c098a398653cba6df0921be21ab50be73a353e
ali <alibabajani1@outlook.com>
2024-09-17 12:56:10 +0330

    added some comments

--- start.py
+++ start.py
@@ -1,7 +1,10 @@
 a = 1
+# here we get a number from user
 size = input("enter a number(it will be 1 to your number):")
+# here we cast size to int
 size = int(size)
 size += 1
 while a < size:
+    # here we print the numbers
     print(a)
     a += 1
